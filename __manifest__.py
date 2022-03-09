587b32ee938482bb753a93b9eaec890c46cf8166
tickernelz <zhafronadani@gmail.com>
2022-03-09 10:37:25 +0700

    create new menu 'Booking'
    Signed-off-by: tickernelz <zhafronadani@gmail.com>

--- __manifest__.py
+++ __manifest__.py
@@ -18,18 +18,14 @@
     'category': 'Sales',
     'version': '1,0',
     'installable': True,
+    'application': True,
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+        'security/ir.model.access.csv',
+        'views/menu.xml',
     ],
 }
--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from . import models
 from . import service_team
 from . import sale_order
 from . import work_order
